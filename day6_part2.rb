0fb37e69628dc331669d2e372cd3b4feac999fc9
eugen neagoe <eneagoe@gmail.com>
2016-12-06 18:43:06 +0200

    Add solution for second part of day 6

---
+++ day6_part2.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby -w
+
+lines = File.open('day6.in').readlines
+puts lines.map { |line| line.chomp.split(//) }.transpose.map { |line| line.sort_by { |c| line.count(c) }.first }.join
+
